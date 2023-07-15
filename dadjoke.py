57a5e3b4671d0446a698a3331b232ed88fd968c3
Paul Nuccio <nuccp@students.fscj.edu>
2023-07-15 10:14:56 -0700

    updated dadjoke script

--- dadjoke.py
+++ dadjoke.py
@@ -13,5 +13,5 @@ response = requests.request("GET", url, headers=headers, data=payload)
 
 responseJson = response.json()
 
-pprint(responseJson)
+print("Your generated joke is: " + str(responseJson['joke']))
 
